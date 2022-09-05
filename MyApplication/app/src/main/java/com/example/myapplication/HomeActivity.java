3dc43f8f2dd19e57b5ec47ef3a5c0f2454952140
Dev-SaTu <tkdwns1610@naver.com>
2022-09-05 23:34:42 +0900

    commit

--- MyApplication/app/src/main/java/com/example/myapplication/HomeActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 import com.example.myapplication.retrofit.RetrofitService;
 import com.example.myapplication.retrofit.UserApi;
+import com.google.zxing.integration.android.IntentIntegrator;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -34,6 +35,21 @@ public class HomeActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(HomeActivity.this, StorePointActivity.class);
                 startActivity(i);
+            }
+        });
+
+        Button buttonCamera = findViewById(R.id.buttonCamera);
+
+        buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                IntentIntegrator qrScan = new IntentIntegrator(HomeActivity.this);
+                qrScan.setOrientationLocked(false); // default가 세로모드인데 휴대폰 방향에 따라 가로, 세로로 자동 변경됩니다.
+                qrScan.setCameraId(1); //후방카메라=0, 전면카메라=1
+                qrScan.setPrompt("<<QR 코드를 스캔하여 주세요>>");
+                qrScan.initiateScan();
+
             }
         });
 
