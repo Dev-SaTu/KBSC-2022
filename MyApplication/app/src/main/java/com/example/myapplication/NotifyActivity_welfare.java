f1a2b8032fe1dcb10366d2102a02ae9c921de503
wch-os <wch1224.s@gmail.com>
2022-09-06 23:36:47 +0900

    commit

--- MyApplication/app/src/main/java/com/example/myapplication/NotifyActivity_welfare.java
+++ MyApplication/app/src/main/java/com/example/myapplication/NotifyActivity_welfare.java
@@ -1,21 +1,18 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class NotifyAcitivy extends AppCompatActivity {
+public class NotifyActivity_welfare extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notify_map);
-
-        Button map = findViewById(R.id.map);
-        map.setOnClickListener();
+        setContentView(R.layout.activity_notify_welfare);
     }
-
-
 }
