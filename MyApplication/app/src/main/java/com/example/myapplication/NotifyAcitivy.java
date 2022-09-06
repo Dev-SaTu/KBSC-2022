c05a9d690974f176e91d87a3ed3bd7aa2937d63a
wch-os <wch1224.s@gmail.com>
2022-09-06 22:24:58 +0900

    PROJECT INIT

--- MyApplication/app/src/main/java/com/example/myapplication/NotifyAcitivy.java
+++ MyApplication/app/src/main/java/com/example/myapplication/NotifyAcitivy.java
@@ -1,4 +1,21 @@
 package com.example.myapplication;
 
-public class NotifyAcitivy {
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NotifyAcitivy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notify_map);
+
+        Button map = findViewById(R.id.map);
+        map.setOnClickListener();
+    }
+
+
 }
