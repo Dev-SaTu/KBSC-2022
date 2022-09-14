826c258f3b3a1c25c8acccf70810e2c28f4eabd5
Goldrichs <thddbr1254@naver.com>
2022-09-14 19:50:01 +0900

    dona

--- kb_spring/SpringServer/src/main/java/com/foch/springserver/model/donate/Donate.java
+++ kb_spring/SpringServer/src/main/java/com/foch/springserver/model/donate/Donate.java
@@ -4,15 +4,15 @@ import java.sql.Timestamp;
 
 public class Donate {
 
-    public int donatePoint;
+    public int accPoint;
 
     public int waitingNumber;
 
     public Timestamp time;
 
-    public void setDonatePoint(int donatePoint){this.donatePoint = donatePoint;}
+    public void setDonatePoint(int accPoint){this.accPoint = accPoint;}
 
-    public int getDonatePoint(){return donatePoint;}
+    public int getDonatePoint(){return accPoint;}
 
     public void setWaitingNumber(int waitingNumber){this.waitingNumber = waitingNumber;}
 
