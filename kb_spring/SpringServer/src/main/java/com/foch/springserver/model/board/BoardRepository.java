5aa159642d220fd51b0c32dc0d1e039bab9a8dc9
Dev-SaTu <tkdwns1610@naver.com>
2022-09-16 00:58:23 +0900

    commit

--- kb_spring/SpringServer/src/main/java/com/foch/springserver/model/board/BoardRepository.java
+++ kb_spring/SpringServer/src/main/java/com/foch/springserver/model/board/BoardRepository.java
@@ -1,9 +1,18 @@
 package com.foch.springserver.model.board;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardRepository extends CrudRepository<Board, String>  {
 
+	@Transactional
+	@Query(value = "SELECT * FROM Board WHERE title like %:title%", nativeQuery = true)
+	List<Board> getBoards();
+	
 }
