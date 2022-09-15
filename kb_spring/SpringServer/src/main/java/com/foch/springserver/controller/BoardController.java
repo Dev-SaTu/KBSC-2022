f2e36a884266c44eb7ad0991a2218de0b6a6396a
Dev-SaTu <tkdwns1610@naver.com>
2022-09-16 00:17:27 +0900

    commit

--- kb_spring/SpringServer/src/main/java/com/foch/springserver/controller/BoardController.java
+++ kb_spring/SpringServer/src/main/java/com/foch/springserver/controller/BoardController.java
@@ -1,8 +1,8 @@
 package com.foch.springserver.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +16,10 @@ public class BoardController {
 	@Autowired
 	private BoardDao boardDao;
 	
-	@GetMapping(value = "/board/")
-	public Board findBoard(@Param("letterNum") int letterNum) {
+	@GetMapping(value = "/board/{letterNum}")
+	public Board findBoard(@PathVariable("letterNum") String letterNum) {
 		
-		return boardDao.find("");
+		return boardDao.find(letterNum);
 		
 	}
 	
